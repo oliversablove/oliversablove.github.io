cd21a9a7fa1a19824021b34fdfe18d0159542383
oliversablove <osablove@gmail.com>
2019-07-22 20:08:44 -0400

    Add Project Containers with Overlay
    -Added project divs with styling.
    -Added animated overlay on project divs using jQuery.
    -Added jQuery script file.

---
+++ script.js
@@ -0,0 +1,17 @@
+(function() {
+
+  'use strict';
+
+  $('.card').mouseenter(function() {
+
+    $(this).find('.card-overlay').css({'top': '-100%'});
+    $(this).find('.card-hover').css({'top': '0'})
+
+  }).mouseleave(function() {
+
+    $(this).find('.card-overlay').css({'top': '0'});
+    $(this).find('.card-hover').css({'top': '100%'})
+
+  })
+  
+})(jQuery);

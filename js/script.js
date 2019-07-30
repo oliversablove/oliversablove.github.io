85ecb2061b984c022063b6fd639c29149482921f
oliversablove <osablove@gmail.com>
2019-07-30 10:44:28 -0400

    Expand Resume Button on Hover

--- js/script.js
+++ js/script.js
@@ -4,16 +4,15 @@ $(() => {
   // project ovrlay
   $('.card').mouseenter(function() {
 
-    $(this).find('.card-overlay').css({'top': '-100%'});
+    $(this).find('.card-overlay').css({'top': '-100%'})
     $(this).find('.card-hover').css({'top': '0'})
 
   }).mouseleave(function() {
 
-    $(this).find('.card-overlay').css({'top': '0'});
+    $(this).find('.card-overlay').css({'top': '0'})
     $(this).find('.card-hover').css({'top': '100%'})
 
   })
 
-  //navbar scroll
-  
+
 })
